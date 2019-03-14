373ea3c8c1cb727805a1c07c728c018096667463
zibs <elizibin@gmail.com>
2019-03-14 10:58:01 -0700

    gesture handler only - no navigation

--- App.js
+++ App.js
@@ -1,12 +1,68 @@
 import React from 'react';
-import { View, Text, TouchableHighlight, TouchableOpacity } from 'react-native';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
+import {
+  Animated,
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight,
+  TouchableOpacity,
+} from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
+// import { createStackNavigator, createAppContainer } from 'react-navigation';
+export class DraggableBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this._translateX = new Animated.Value(0);
+    this._translateY = new Animated.Value(0);
+    this._lastOffset = { x: 0, y: 0 };
+    this._onGestureEvent = Animated.event(
+      [
+        {
+          nativeEvent: {
+            translationX: this._translateX,
+            translationY: this._translateY,
+          },
+        },
+      ],
+      { useNativeDriver: true },
+    );
+  }
+  _onHandlerStateChange = event => {
+    if (event.nativeEvent.oldState === State.ACTIVE) {
+      this._lastOffset.x += event.nativeEvent.translationX;
+      this._lastOffset.y += event.nativeEvent.translationY;
+      this._translateX.setOffset(this._lastOffset.x);
+      this._translateX.setValue(0);
+      this._translateY.setOffset(this._lastOffset.y);
+      this._translateY.setValue(0);
+    }
+  };
+  render() {
+    return (
+      <PanGestureHandler
+        {...this.props}
+        onGestureEvent={this._onGestureEvent}
+        onHandlerStateChange={this._onHandlerStateChange}>
+        <Animated.View
+          style={[
+            styles.box,
+            {
+              transform: [{ translateX: this._translateX }, { translateY: this._translateY }],
+            },
+            this.props.boxStyle,
+          ]}
+        />
+      </PanGestureHandler>
+    );
+  }
+}
 
 class HomeScreen extends React.Component {
   render() {
     return (
       <View testID="welcome" style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>Home Screen</Text>
+        <DraggableBox />
 
         <TouchableOpacity testID="hello_button">
           <Text>again</Text>
@@ -16,10 +72,25 @@ class HomeScreen extends React.Component {
   }
 }
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen,
+// const AppNavigator = createStackNavigator({
+//   Home: {
+//     screen: HomeScreen,
+//   },
+// });
+
+// export default createAppContainer(AppNavigator);
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  scrollView: {
+    flex: 1,
+  },
+  box: {
+    width: 150,
+    height: 150,
+    alignSelf: 'center',
+    backgroundColor: 'plum',
+    margin: 10,
+    zIndex: 200,
   },
 });
-
-export default createAppContainer(AppNavigator);
